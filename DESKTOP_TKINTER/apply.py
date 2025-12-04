48c740e08713c9830c4b0fd4b184fd948fd01962
bouinloig-code <bouin.loig@gmail.com>
2025-12-04 11:05:43 +0100

    sheessh

--- DESKTOP_TKINTER/apply.py
+++ DESKTOP_TKINTER/apply.py
@@ -8,7 +8,18 @@ class App(tk.Tk):
     def __init__(self):
         """ Application constructor (heritage=Tk object) """
         super().__init__()
-
+        self.screen_h = 880 #self.winfo_screenheight()
+        self.screen_v = self.winfo_screenheight()
+        self.screen_x = 50
+        self.screen_y = 0
+        geometry = str(self.screen_h) + "x" + str(self.screen_v) + "+" + str(self.screen_x) + "+" + str(self.screen_y)
+        self.geometry(geometry)
+        self.resizable(True, True) # (width, height)
+        self.minsize(100, 100)
+        self.maxsize(self.winfo_screenwidth(), self.winfo_screenheight())
+        self.attributes('-alpha', 0.9) #window transparency
+        self.iconbitmap(Path("image/logo.xbm"))
+        self.title("Desktop GUI")
 #====================================================================
 
 if __name__ == "__main__":
