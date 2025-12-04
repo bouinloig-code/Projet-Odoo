0f4f8302a2506c674a65e6988a52b1ad74368350
bouinloig-code <bouin.loig@gmail.com>
2025-12-04 09:48:53 +0100

    commit Update apply.py and organize images

--- DESKTOP_TKINTER/apply.py
+++ DESKTOP_TKINTER/apply.py
@@ -1,4 +1,16 @@
+from pathlib import Path
 import tkinter as tk
 
-root = tk.Tk()
-root.mainloop()
+#====================================================================
+
+class App(tk.Tk):
+    """ Application GUI in Tkinter"""
+    def __init__(self):
+        """ Application constructor (heritage=Tk object) """
+        super().__init__()
+
+#====================================================================
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
